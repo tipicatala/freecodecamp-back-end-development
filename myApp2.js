5a1005dead0e2c4217ca753e72f5650532286259
Платонова Дарья Андреевна <plat-dasha@mail.ru>
2021-09-30 20:02:46 +0300

    completed mongo db and mongoose

--- myApp2.js
+++ myApp2.js
@@ -67,23 +67,43 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName},
+    { age: ageToSet },
+    {new: true},
+    (err, data) => {
+      if(err) return console.log(err);
+      return done(null, data)
+    })
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(
+    personId,
+    (err, data) => {
+      if (err) return console.error(err)
+      return done(null, data);
+    } 
+  )
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, data) => {
+      if (err) return console.error(err)
+      return done(null, data);
+  })
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+  .sort({ name: 'asc' })
+  .limit(2)
+  .select({ age: 0 })
+  .exec(function(error, data) {
+    return done(null, data);
+  });
 };
 
 /** **Well Done !!**
